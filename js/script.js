33805b6996c7098a25b451585de0d4f795fd44a5
shalom <maminpahat@gmail.com>
2020-10-03 21:22:06 +0300

    new animations were added

--- js/script.js
+++ js/script.js
@@ -7,10 +7,14 @@ $(document).ready(function() {
     }
   }
 
-  function showExample(buttonName, codeBoxName) {
+  function showExample(buttonName, arrowName, codeBoxName, gearsName, resultName, title1, title2) {
     $(buttonName).on('click', function(event){
       $(codeBoxName).toggleClass('code__box_active wow fadeInLeft');
-      $(buttonName).toggleClass('button_active');
+      $(arrowName).toggleClass('arrow__active');
+      $(gearsName).toggleClass('gears__block_active');
+      $(resultName).toggleClass('code__box_active wow fadeInLeft');
+      $(title1).toggleClass('h-title');
+      $(title2).toggleClass('h-title');
     });
   }
 
@@ -23,7 +27,9 @@ $(document).ready(function() {
     }
   }
 
-  addDelay(delay);
-  stringNumber('#code__box_1', '.code__box__item', '.string__number');
-  showExample('#add_content_button_1', '#code__box_1');
+  showExample('#add_content_button_1', '#arrow_1', '#code__box_1', '.gears__block', '#code__box_2', '#h-title_1', '#h-title_2'); //show the code-box, titles and gears; reverse the arrow and toggle item's position
+  addDelay(delay); //make a delay of the animation
+
+  stringNumber('#code__box_1', '.code__box__item', '.string__number'); //set a number of a string of the code-box
+  stringNumber('#code__box_2', '.code__box__item', '.string__number');
 });
